47a7aa53b672b041ad084f31524b098f0bbf1569
Mikhail dJoker Orlov <mix.djoker@gmail.com>
2023-10-01 19:42:53 +0300

    pr: fix

--- internal/lib/handler/chat_v1.go
+++ internal/lib/handler/chat_v1.go
@@ -43,7 +43,7 @@ func (s *ChatRPCServerV1) Create(ctx context.Context, req *desc.CreateRequest) (
 		s.log.Println(color.BlueString("Deadline: %v", dline))
 	}
 
-	randInt64, err := rand.Int(rand.Reader, new(big.Int).SetInt64(1<<63-1))
+	randInt64, err := rand.Int(rand.Reader, new(big.Int).SetInt64(1<<62))
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
